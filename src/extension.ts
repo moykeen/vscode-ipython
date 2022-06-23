a1ef4760704cc5bedb91f801d9adc00eafd1eb96
moykeen <31440325+moykeen@users.noreply.github.com>
2022-06-23 23:39:36 +0900

    use active terminal

--- src/extension.ts
+++ src/extension.ts
@@ -232,11 +232,14 @@ export function activate(context: vscode.ExtensionContext) {
 		let name = makeTerminalName(terminalName);
 		let terminals = vscode.window.terminals;
 		if (terminals.length > 0) {
-			for (let i = terminals.length - 1; i >= 0; i--) {
-				if (terminals[i].name === name) {
-					return terminals[i];
+			if (uniqueTerminals) {
+				for (let i = terminals.length - 1; i >= 0; i--) {
+					if (terminals[i].name === name) {
+						return terminals[i];
+					}
 				}
 			}
+			return vscode.window.activeTerminal!;
 		}
 		return await createTerminal(name);
 	}
